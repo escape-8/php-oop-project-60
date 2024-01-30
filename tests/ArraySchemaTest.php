0ce9b66bedf268e153b3f1e313c4e65687991f3f
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-01-30 13:25:45 +0300

    feat: tests for array schema

---
+++ tests/ArraySchemaTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Hexlet\Code\Tests;
+
+use Hexlet\Code\Schemas\ArraySchema;
+use Hexlet\Code\Validator;
+use PHPUnit\Framework\TestCase;
+
+class ArraySchemaTest extends TestCase
+{
+    private ArraySchema $arraySchema;
+
+    public function setUp(): void
+    {
+        $validator = new Validator();
+        $this->arraySchema = $validator->array();
+    }
+
+    public function testRequiredTrue(): void
+    {
+        $result = $this->arraySchema->required()->isValid([]);
+        $this->assertTrue($result);
+    }
+
+    public function testRequiredFalse(): void
+    {
+        $result = $this->arraySchema->required()->isValid(null);
+        $this->assertFalse($result);
+    }
+
+    public function testRequiredDefault(): void
+    {
+        $result = $this->arraySchema->isValid(null);
+        $this->assertTrue($result);
+    }
+
+    public function testSizeOfTrue(): void
+    {
+        $result = $this->arraySchema->sizeof(2)->isValid([1, 'two']);
+        $this->assertTrue($result);
+    }
+
+    public function testSizeOfFalse(): void
+    {
+        $result = $this->arraySchema->sizeof(2)->isValid(['two']);
+        $this->assertFalse($result);
+    }
+
+    public function testMultipleCallTrue(): void
+    {
+        $result = $this->arraySchema->required()->sizeof(2)->isValid(['one', 'two']);
+        $this->assertTrue($result);
+    }
+
+    public function testMultipleCallFalse(): void
+    {
+        $result = $this->arraySchema->required()->sizeof(2)->isValid(['one']);
+        $this->assertFalse($result);
+    }
+
+}
