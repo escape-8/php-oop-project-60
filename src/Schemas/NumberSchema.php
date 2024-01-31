d59dcaf700e97bd76f44136ced2c76a89021edf4
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-01-31 15:29:34 +0300

    refactor: change return in required func in NumberSchema

--- src/Schemas/NumberSchema.php
+++ src/Schemas/NumberSchema.php
@@ -7,7 +7,7 @@ class NumberSchema extends Schema
     public function __construct($validators = [], $checks = [], $checksArgs = [], $requiredValue = false)
     {
         $this->validators = array_merge([
-            'required' => fn(int|null $integer) => !is_int($integer),
+            'required' => fn(int|null $integer) => ($integer === null) || ($integer === 0),
             'positive' => fn(int|null $integer) => $integer >= 0,
             'range' => fn(int|null $integer, int $from, int $to) => ($integer >= $from) && ($integer <= $to),
         ], $validators);
