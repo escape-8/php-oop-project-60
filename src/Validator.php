730eee71bee28eba2b59115a378037a540a2d682
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-01-31 13:43:49 +0300

    feat: implement addValidator func

--- src/Validator.php
+++ src/Validator.php
@@ -8,18 +8,30 @@ use Hexlet\Code\Schemas\StringSchema;
 
 class Validator
 {
+    private array $userValidators = [];
     public function string(): StringSchema
     {
-        return new StringSchema();
+        $stringUserValidators = $this->userValidators['string'] ?? [];
+        return new StringSchema($stringUserValidators);
     }
 
     public function number(): NumberSchema
     {
-        return new NumberSchema();
+        $numberUserValidators = $this->userValidators['number'] ?? [];
+        return new NumberSchema($numberUserValidators);
     }
 
     public function array(): ArraySchema
     {
-        return new ArraySchema();
+        $arrayUserValidators = $this->userValidators['array'] ?? [];
+        return new ArraySchema($arrayUserValidators);
+    }
+
+    public function addValidator(string $type, string $name, callable $fn): void
+    {
+        if (!method_exists($this, $type)) {
+            throw new \InvalidArgumentException('Type of validator ' . $type . ' does not exists');
+        }
+        $this->userValidators[$type][$name] = $fn;
     }
 }
